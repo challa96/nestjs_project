0a1d068df05a31472ce5e0e24969698169ab7cc1
m.ifthekar <m.ifthekar@jazeeratech.in>
2020-09-01 19:04:32 +0530

    express example for graphql

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const schema = require('./schema/schema');
+
+const app = express();
+
+app.use('/graphql',graphqlHTTP({
+	schema,
+	graphiql:true 
+}));
+
+
+
+app.listen(4000,()=>{
+	console.log("serve rrunning on port",4000);
+});
+
